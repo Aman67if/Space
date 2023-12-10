68ffb369f3df41b0cb8b2fea3837454b4ac29fdb
Aman <www.opchoudhary786@gmail.com>
2023-12-10 09:10:58 +0000

    first commit

---
+++ components/main/Navbar.tsx
@@ -0,0 +1,32 @@
+import { Socials } from '@/constants'
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 md:px-10 pl-[5px] pr-[55px]'>
+        <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
+            <a href="#about-me" className='h-auto w-auto flex flex-row items-center'>
+                <Image src='/NavLogo.png' alt='logo' height={70} width={70} className='cursor-pointer hover:animate-slowspin'/>
+                <span className='font-bold ml-[10px] hidden md:block text-gray-300'>
+                    Welcome To Space
+                </span>
+            </a>
+            <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
+                <div className='w-full h-auto flex items-center justify-between border border-[#7042F861] bg-[#0300145E] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
+                    <a href="#about-me" className='cursor-pointer'>About me</a>
+                    <a href="#skills" className='cursor-pointer'>Skills</a>
+                    <a href="#projects" className='cursor-pointer'>Projects</a>
+                </div>
+            </div>
+            <div className='flex flex-row gap-5'>
+                {Socials.map((social) => (
+                    <Image src={social.src} alt={social.name} key={social.name} height={24} width={24}/>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
